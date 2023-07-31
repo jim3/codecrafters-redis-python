088a9ad199649b33727a6795b962b27b1f52b101
jim3 <jimmy3.xyz@gmail.com>
2023-07-31 17:42:18 -0400

    PONG stage

--- app/main.py
+++ app/main.py
@@ -10,7 +10,6 @@ def socket_req(clientsocket):
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept()  # wait for client
     (clientsocket, address) = server_socket.accept()  # wait for client
     socket_req(clientsocket)
 
