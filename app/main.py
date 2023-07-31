24d4c46377300b766a730ebb2f8cda5a7a12d10c
jim3 <jimmy3.xyz@gmail.com>
2023-07-31 16:10:06 -0400

    PONG stage

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept()  # wait for client
     server_socket.sendall(b"+PONG\r\n")
-    server_socket.close()
 
 
 if __name__ == "__main__":
