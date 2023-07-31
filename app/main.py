cf92dd5a9e3405edb63ae377b7eebbc88f273aae
jim3 <jimmy3.xyz@gmail.com>
2023-07-31 16:09:23 -0400

    PONG stage

--- app/main.py
+++ app/main.py
@@ -4,19 +4,8 @@ import socket
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    try:
-        while True:
-            (clientsocket, address) = server_socket.accept()  # wait for client
-            request = clientsocket.recv(1024).decode("utf-8").strip()
-            if request == "PING":
-                response = "+PONG\r\n"
-            else:
-                response = "-Error message\r\n"
-            clientsocket.sendall(response.encode("utf-8"))
-            clientsocket.close()
-
-    except KeyboardInterrupt:
-        pass
+    server_socket.accept()  # wait for client
+    server_socket.sendall(b"+PONG\r\n")
     server_socket.close()
 
 
